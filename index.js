65edec18388990b25fbd52e5dbbd04d083a9593f
Petchdy <skulwisit@gmail.com>
2026-03-16 15:35:08 +0700

    fix_back

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const http = require('http');
 const requestListener = (req, res) => { 
     res.statusCode = 200; 
     res.setHeader('Content-Type', 'text/plain'); 
-    res.end('Hello orld from DevOps Lab!\n'); 
+    res.end('Hello World from DevOps Lab!\n'); 
 }; 
 
 const app = http.createServer(requestListener); 
 // We only listen if this file is run directly, not when imported by tests
-
+ 
 if (require.main === module) { 
     const port = process.env.PORT || 3000; 
     app.listen(port, () => { 
