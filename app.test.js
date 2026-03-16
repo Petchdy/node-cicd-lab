13ab4bcdaaf67c76beee98a8d58af0a34bdbda86
Petchdy <skulwisit@gmail.com>
2026-03-16 15:17:00 +0700

    fix

--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest'); 
-const app = require('./index'); 
+const app = require('/index'); 
  
 describe('GET /', () => { 
   it('should return 200 OK and Hello World message', async () => { 
